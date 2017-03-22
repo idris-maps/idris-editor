861ee2772763c0029029f8b129eb01c87b01cfae
Idris <idris.maps@gmx.ch>
2017-03-22 11:11:23 +0100

    meta

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
 		]),
 		new webpack.DefinePlugin({
 				'process.env': {
-				  NODE_ENV: JSON.stringify('developement')
+				  NODE_ENV: JSON.stringify('production')
 				}
 		}),
 		new UglifyJsPlugin({cacheFolder: 'cache'})
